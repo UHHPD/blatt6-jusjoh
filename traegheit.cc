ecfbedf6785edacbda1ab5f3f0a518f583af1d3d
jwilckens <102976232+jwilckens@users.noreply.github.com>
2022-06-20 15:19:06 +0000

    Berarbeitung des Arbeitsblatts. Abgabe der Tabelle über Moodle

--- traegheit.cc
+++ traegheit.cc
@@ -6,7 +6,7 @@
 
 int main() {
   const int N = 10000;     // Anzahl Integrationspunkte
-  const double M = 1;      // Masse des Zylindermantels
+  const double M = 2;      // Masse des Zylindermantels
   const double ZM_R = 1.0; // Radius der Zylindermantels
   const double ZM_L = 1.0; // Laenge des Zylindermantels
 
@@ -18,20 +18,28 @@ int main() {
   std::cin >> u;
 
   std::unique_ptr<Zylindermantel> zm(new Zylindermantel(ZM_R, ZM_L));
+  std::unique_ptr<Vollzylinder> vz(new Vollzylinder(ZM_R, ZM_L));
 
-  double J = 0;     // Massentraegheitsmoment
+  double Jzm = 0;     // Massentraegheitsmoment
+  double Jvz = 0;
   double m = M / N; // Masse eines Massenpunktes
   for (int i = 0; i < N; ++i) {
     Vektor x = zm->punkt();
+    Vektor y = vz->punkt();
     // Abstand Punkt x und Gerade a + t*u
-    // Vektor n = ...;//Normalenvektor x-a kreuz u
-    double r = 0; //|n|/|u|
+    Vektor n = (x-a).kreuz(u);//Normalenvektor x-a kreuz u
+    Vektor nvz = (y-a).kreuz(u);
+    double r = n.betrag()/u.betrag(); //|n|/|u|
+    double rvz = nvz.betrag()/u.betrag();
     // std::cout << x << " :" << r << std::endl;
     // addiere Beitrag des Massenpunktes zum Traegheitsmoment
-    J += m * r * r;
+    Jzm += m * r * r;
+    Jvz += m * rvz * rvz;
   }
   std::cout << "Massentraegheitsmoment fuer einen Zylindermantel"
-            << " mit a = " << a << " und u = " << u << ": " << J << std::endl;
+            << " mit a = " << a << " und u = " << u << ": " << Jzm << std::endl;
+  std::cout << "Massentraegheitsmoment fuer einen Vollzylinder"
+            << " mit a = " << a << " und u = " << u << ": " << Jvz << std::endl;
             
   return 0;
 }
